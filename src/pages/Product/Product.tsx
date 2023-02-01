3c5405168899911908548d744805eeda7ab3e81b
Omkar75 <omkarzagade75@gmail.com>
2023-02-01 21:26:08 +0530

    working on product page

--- src/pages/Product/Product.tsx
+++ src/pages/Product/Product.tsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Product.scss"
 const Product = () => {
+  const [selectedImage, setSelectedImage] = useState(0)
+  const images = [
+    "https://images.pexels.com/photos/10026453/pexels-photo-10026453.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    "https://images.pexels.com/photos/12179283/pexels-photo-12179283.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  ]
   return (
-    <div className='product'>Product</div>
+    <div className='product'>
+      <div className="left">
+        <div className="images">
+          <img src={images[0]} alt="" onClick={e=>setSelectedImage(0)}/>
+          <img src={images[1]} alt="" onClick={e=>setSelectedImage(1)}/>
+        </div>
+        <div className="mainImg">
+          <img src={images[selectedImage]} alt="" />
+        </div>
+      </div>
+      <div className="right"></div>
+    </div>
   )
 }
 
